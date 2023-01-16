940315b77cac6d558fc7d5bcd71c10605641fa11
Joseph Hilby <joseph.hilby@gmail.com>
2023-01-16 09:34:58 -0600

    Add validations for favorite objects

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-    describe 'relationships' do
+  describe 'validations' do
+    it { should validate_presence_of(:country) }
+    it { should validate_presence_of(:recipe_link) }
+    it { should validate_presence_of(:recipe_title) }
+  end
+  describe 'relationships' do
     it { should have_many(:users_favorites) }
     it { should have_many(:users).through(:users_favorites) }
   end
