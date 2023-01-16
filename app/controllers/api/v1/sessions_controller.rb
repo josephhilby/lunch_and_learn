1ea7608ef658d9a7b225e694083fd21953c7b23c
Joseph Hilby <joseph.hilby@gmail.com>
2023-01-16 09:36:32 -0600

    Updated error handling

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,16 +5,16 @@ module Api
       def create
         user = User.find_by(email: user_params[:email])
         if user && user.authenticate(user_params[:password])
-          render json: UserSerializer.new(user)
+          render json: UserSerializer.new(user), status: 201
         else
-          render json: { message: "Unknown username or password" }, status: 400
+          render json: { message: "Unknown username or password" }, status: 401
         end
       end
 
       private
 
       def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        params.permit(:name, :email, :password, :password_confirmation)
       end
     end
   end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,7 @@ module Api
         # this is not a good solution, need to find a user unique way of generating key
         new_user[:api_key] = SecureRandom.base64
         if new_user.save
-          render json: UserSerializer.new(new_user)
+          render json: UserSerializer.new(new_user), status: 201
         else
           render json: { message: new_user.errors.full_messages.to_sentence }, status: 400
         end
@@ -16,7 +16,7 @@ module Api
       private
 
       def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        params.permit(:name, :email, :password, :password_confirmation)
       end
     end
   end
