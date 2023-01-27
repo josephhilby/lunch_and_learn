57e0c7c5efc9f096f509eac851511eac8f9e71af
Naomi Yocum <naomiyocum24@gmail.com>
2023-01-27 09:09:50 -0700

    Fix texting to match url change

--- spec/poros/learning_resource_spec.rb
+++ spec/poros/learning_resource_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe LearningResource do
 	before do
 		country = Country.new('name')
-		images = [ Image.new({ alt: 'alt', url: 'url' }),
-               Image.new({ alt: 'alt', url: 'url' }) ]
+		images = [ Image.new({ alt: 'alt', src: {original: 'org'} }),
+               Image.new({ alt: 'alt', src: {original: 'org'} }) ]
 		video = Video.new({ snippet: { title: 'title' },
                         id: { videoId: 'videoId' } })
 
@@ -19,7 +19,7 @@ RSpec.describe LearningResource do
 		it 'has readible attributes' do
       country_return = 'name'
       video_return = { title: 'title', youtube_video_id: 'videoId' }
-      images_return = [{ alt_tag: 'alt', url: 'url' }, { alt_tag: 'alt', url: 'url' }]
+      images_return = [{:alt_tag=>"alt", :url=>"org"}, {:alt_tag=>"alt", :url=>"org"}]
 			expect(@learning_resource.country).to eq(country_return)
 			expect(@learning_resource.video).to eq(video_return)
 			expect(@learning_resource.images).to eq(images_return)
