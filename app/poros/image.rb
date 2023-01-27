c2d05d0f0ea3ba4394ba648be6eaa1728badfa9b
Naomi Yocum <naomiyocum24@gmail.com>
2023-01-26 18:11:35 -0700

    Change image url to grab original size

--- app/poros/image.rb
+++ app/poros/image.rb
@@ -2,6 +2,6 @@ class Image
 	attr_reader :alt_tag, :url
 	def initialize(attributes)
 		@alt_tag = attributes[:alt]
-		@url = attributes[:src][:tiny]
+		@url = attributes[:src][:original]
 	end
 end
