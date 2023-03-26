060bf941d50b4ea66f962314a7f59979cb266d70
Joseph Hilby <joseph.hilby@gmail.com>
2023-03-26 08:53:41 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'test', email: 'test@test', api_key: 'test_key', password: 'test', password_confirmation: 'test')
+title = "Andy Ricker's Naam Cheuam Naam Taan Piip (Palm Sugar Simple Syrup)"
+url = "http://www.edamam.com/ontologies/edamam.owl#recipe_3a551652abf5a1cef1421660d1b657d9"
+
+user = User.create(name: 'test', email: 'test@test', api_key: 'test_key', password: 'test', password_confirmation: 'test')
+favorite_1 = Favorite.create(country: 'test_country', recipe_link: 'test_link', recipe_title: 'test_title')
+favorite_2 = Favorite.create(country: 'Thailand', recipe_link: url, recipe_title: title)
+UsersFavorite.create(user_id: user.id, favorite_id: favorite_1.id)
+UsersFavorite.create(user_id: user.id, favorite_id: favorite_2.id)
